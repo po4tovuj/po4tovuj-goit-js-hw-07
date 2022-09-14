21174fa24f5061785098833fe86192325b720769
Sergey Miniaylo <po4tovuj@gmail.com>
2022-09-14 14:33:29 +0300

    done with gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const images = galleryItems.reduce(
+  (acc, { preview, description, original }) => {
+    acc += `<a class="gallery__link" href="#" target="_self"> <img  class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /> </a>`;
+    return acc;
+  },
+  ''
+);
+let instance;
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('afterbegin', '<ul></ul>');
+container.insertAdjacentHTML(
+  'afterbegin',
+  `  ${images} </a>
+`
+);
+
+const closeOnEcapeClick = (e) => {
+  if (instance.visible() && e.key === 'Escape') {
+    instance.close();
+    container.removeEventListener('keydown', closeOnEcapeClick);
+  }
+};
+
+const showModal = ({ target, currentTarget }) => {
+  console.log(target.nodeName);
+  if (target.nodeName === 'IMG') {
+    const url = target.getAttribute('data-source');
+    const alt = target.getAttribute('alt');
+    instance = basicLightbox.create(
+      `<img width="800", heigth="600" src=${url} alt=${alt}>`,
+      {
+        onShow: (instance) => {
+          container.addEventListener('keydown', closeOnEcapeClick);
+        },
+        onClose: (instance) => {
+          container.removeEventListener('keydown', closeOnEcapeClick);
+        },
+      }
+    );
+    instance.show();
+  }
+};
+
+// To remove the listener, use the normal routine:
+container.addEventListener('click', showModal);
