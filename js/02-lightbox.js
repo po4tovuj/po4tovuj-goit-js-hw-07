78ad3a6dc7b0e98d3e5e856deb91c235092e7a33
Sergey Miniaylo <po4tovuj@gmail.com>
2022-09-14 14:57:55 +0300

    done with simppleLightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const images = galleryItems.reduce(
+  (acc, { preview, description, original }) => {
+    acc += `<li class="gallery__item">
+<a class="gallery__link" href="${original}"> <img  class="gallery__image" src="${preview}"  alt="${description}" /> </a> </li>`;
+    return acc;
+  },
+  ''
+);
+let instance;
+
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('afterbegin', '<ul></ul>');
+container.insertAdjacentHTML(
+  'afterbegin',
+  `  ${images} </a>
+`
+);
+
+const lightBox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightBox.on('show.simplelightbox');
 
 console.log(galleryItems);
